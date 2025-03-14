7cea7a49a018c881ca88338ad5309934ea27385c
Kayodê Vital <kayode.vital@tutanota.com>
2025-03-14 19:36:21 -0300

    fixed merger conflicts

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -6,16 +6,9 @@ if [ "$rc" == 0 ]; then
 /bin/redshift-gtk &
 fi
 nitrogen --restore &
-<<<<<<< HEAD
-##mintupdate-launcher &
-nm-applet &
-##xfce4-timer-plugin &
-=======
-#/bin/redshift-gtk &
 #mintupdate-launcher &
 nm-applet &
 #xfce4-timer-plugin &
->>>>>>> babc4b358bb7873567320a4c9a93007482bb028a
 slstatus &
 xfce4-power-manager &
 vc=$(pgrep -l volumeicon | wc -l)
@@ -25,18 +18,9 @@ volumeicon &
 fi
 xset r rate 300 50 &
 sxhkd &
-<<<<<<< HEAD
 flameshot -t false &
-##syncthing &
+#syncthing &
 blueman-applet &
-#sc=$(pgrep -l librewolf | wc -l)
+sc=$(pgrep -l librewolf | wc -l)
 #if [ "$sc" == 1]; then
 #fi
-=======
-flameshot &
-kc=$(pgrep -l kdeconnect-app | wc -l)
-if [ "$kc" == 0 ]; then
-kdeconnect-app &
-syncthing &
-fi
->>>>>>> babc4b358bb7873567320a4c9a93007482bb028a
